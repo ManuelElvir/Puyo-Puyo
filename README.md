2872566af7099af942146815a57b0984d671efc5
ManuelElvir <manuel.njiakim@gmail.com>
2023-09-25 13:00:56 +0200

    combo feature

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ This is a simple React version of the single-player mode of the game Puyo Puyo (
    npm run dev
 ```
 
+## How it works
+
+use your mouse wheel to rotate the block in either direction; use the arrow keys to move the block left or right.
+
+Click on the game screen or use the down arrow to bring the block down instantly.
+
+NB : Don't forget to focus on the screen to use the arrow keys
+
 ## Demo
 
 ![Capture 1](public/img/cap1.png)
--- src/functions/puyo.ts
+++ src/functions/puyo.ts
@@ -1,5 +1,33 @@
 import { POS, PUYO } from "../types/puyo";
 
+export const reordering = (puyos: PUYO[]) => {
+    let hasChanged = false;
+    // // push all last puyos to the bottom
+    puyos.forEach((puyo) => {
+        if (isTheDeepest(puyo, puyos)) {
+            // console.log('is deepest', puyo)
+            puyo.pos.y = 11
+            // puyos[index] = puyo
+        }
+    })
+
+    // reorder all other puyos
+    let i = 10;
+    do {
+        puyos.forEach((puyo) => {
+            if (puyo.pos.y === i) {
+                puyo.pos.y = getDeepestAvailablePosition(puyo.pos.x, puyos.filter(item => item.pos.y > puyo.pos.y))
+                // puyos[index] = puyo
+            }
+        })
+        i--;
+    } while (i >= 0)
+
+    // console.log('after order', puyos)
+
+    return hasChanged
+}
+
 export const calculateScore = (puyos: PUYO[]) => {
     let score = 0;
 
@@ -9,90 +37,96 @@ export const calculateScore = (puyos: PUYO[]) => {
     let combo: PUYO[] = [];
     let currentType = null;
 
-    // on parcour horizontalement
-    for (let i = ROWS - 1; i >= 0; i--) {
-        for (let j = 0; j < COLS; j++) {
-            const currentPuyo = puyos.find(puyo => puyo.pos.x === j && puyo.pos.y === i)
-            if (!currentPuyo) {
-                if (combo.length >= 3) {
-                    puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
-                    score = 50 * combo.length
-                }
-                combo = []
-                continue;
-            }
 
-            if (!currentType) {
-                combo.push(currentPuyo)
-
-                // now check around this position
-                currentType = currentPuyo.type
-            }
-            else {
-                if (currentPuyo.type === currentType) {
-                    combo.push(currentPuyo)
-                }
-                else {
-                    if (combo.length >= 3) {
-                        puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
-                        score = 50 * combo.length
-                    }
-                    combo = []
-                    currentType = currentPuyo.type
-                    combo.push(currentPuyo)
-                }
-            }
-        }
-    }
-
-    // On parcour verticalement
-    for (let j = 0; j < COLS; j++) {
+    let hasChanged = false;
+    do {
+        // on parcour horizontalement
         for (let i = ROWS - 1; i >= 0; i--) {
-            const currentPuyo = puyos.find(puyo => puyo.pos.x === j && puyo.pos.y === i)
-            if (!currentPuyo) {
-                if (combo.length >= 3) {
-                    puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
-                    score = 50 * combo.length
-                }
-                combo = []
-                continue;
-            }
-
-            if (!currentType) {
-                combo.push(currentPuyo)
-
-                // now check around this position
-                currentType = currentPuyo.type
-            }
-            else {
-                if (currentPuyo.type === currentType) {
-                    combo.push(currentPuyo)
-                }
-                else {
+            for (let j = 0; j < COLS; j++) {
+                const currentPuyo = puyos.find(puyo => puyo.pos.x === j && puyo.pos.y === i)
+                if (!currentPuyo) {
                     if (combo.length >= 3) {
-                        
                         puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
                         score = 50 * combo.length
                     }
                     combo = []
-                    currentType = currentPuyo.type
+                    continue;
+                }
+
+                if (!currentType) {
                     combo.push(currentPuyo)
+
+                    // now check around this position
+                    currentType = currentPuyo.type
+                }
+                else {
+                    if (currentPuyo.type === currentType) {
+                        combo.push(currentPuyo)
+                    }
+                    else {
+                        if (combo.length >= 3) {
+                            puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
+                            score = 50 * combo.length
+                        }
+                        combo = []
+                        currentType = currentPuyo.type
+                        combo.push(currentPuyo)
+                    }
                 }
             }
         }
-    }
 
-    return {nextPuyos: puyos, nextScore: score};
+        // On parcour verticalement
+        for (let j = 0; j < COLS; j++) {
+            for (let i = ROWS - 1; i >= 0; i--) {
+                const currentPuyo = puyos.find(puyo => puyo.pos.x === j && puyo.pos.y === i)
+                if (!currentPuyo) {
+                    if (combo.length >= 3) {
+                        puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
+                        score = 50 * combo.length
+                    }
+                    combo = []
+                    continue;
+                }
+
+                if (!currentType) {
+                    combo.push(currentPuyo)
+
+                    // now check around this position
+                    currentType = currentPuyo.type
+                }
+                else {
+                    if (currentPuyo.type === currentType) {
+                        combo.push(currentPuyo)
+                    }
+                    else {
+                        if (combo.length >= 3) {
+
+                            puyos = puyos.filter(puyo => combo.findIndex(puyoItem => puyo.id === puyoItem.id) === -1)
+                            score = 50 * combo.length
+                        }
+                        combo = []
+                        currentType = currentPuyo.type
+                        combo.push(currentPuyo)
+                    }
+                }
+            }
+        }
+        hasChanged = reordering(puyos)
+    } while (hasChanged)
+
+
+    return { nextPuyos: puyos, nextScore: score };
 };
 
 export const canContinuePlaying = (puyos: PUYO[], pos1: POS, pos2: POS) => {
-    const puyosAtIndex0 = puyos.findIndex(puyo => 
+    const puyosAtIndex0 = puyos.findIndex(puyo =>
         (puyo.pos.y === 0 && pos1.x === puyo.pos.x) ||
         (puyo.pos.y === 0 && pos2.x === puyo.pos.x) ||
         (puyo.pos.y < 0)
     )
 
-    if(puyosAtIndex0==-1) return true;
+    if (puyosAtIndex0 == -1) return true;
     return false
 }
 
@@ -121,4 +155,30 @@ export const randomPosition2 = (pos: POS) => {
         return { x: pos.x - 1, y: pos.y }
     }
     return { x: pos.x + 1, y: pos.y }
+}
+
+
+
+const isTheDeepest = (puyo: PUYO, puyos: PUYO[]) => {
+    const deepestPuyo = puyos.find(item => item.pos.x === puyo.pos.x && item.pos.y === getDeepestPosition(puyo.pos.x, puyos))
+    if (deepestPuyo && deepestPuyo.id === puyo.id) return true;
+    return false;
+}
+
+const getDeepestPosition = (x: number, puyos: PUYO[]) => {
+    const posY = puyos.filter(item => item.pos.x === x).reduce(function (lastDeepestPosition, currentItem) {
+        if (currentItem.pos.y > lastDeepestPosition) return currentItem.pos.y
+        else return lastDeepestPosition
+    }, 0)
+    return posY
+}
+
+const getDeepestAvailablePosition = (x: number, puyos: PUYO[]) => {
+    // console.log('filtered puyos', puyos);
+
+    const posY = puyos.filter(item => item.pos.x === x).reduce(function (lastDeepestPosition, currentItem) {
+        if (currentItem.pos.y < lastDeepestPosition) return currentItem.pos.y
+        else return lastDeepestPosition
+    }, 12)
+    return posY - 1
 }
